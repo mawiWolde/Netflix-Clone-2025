07829f9c21fd07d11cfc8c7d03f03d6012deff31
Kedamawi <eyasu10@proton.me>
2025-09-12 17:45:08 -0500

    gh page, dir name fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
-//import path from path to be able use it bellow
 import path from "path";
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/Netflix-Clone-2025/",
-  plugins: [tailwindcss(), react()],
+import { fileURLToPath } from "url";
 
-  //set src path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  base: "/Netflix-Clone-2025/", // repo name, trailing slash required
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
